b1974f3795ac7b6c002d1b63231937fddac7e5f9
Pedro e Maria Júlia <shiro0321169@gmail.com>
2022-09-17 08:51:15 -0300

    Alterando projeto html para jsx

--- pages/index.js
+++ pages/index.js
@@ -1,59 +1,80 @@
-import Head from 'next/head';
-import styles from '../styles/Home.module.css';
-
-export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+var NewComponent = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <title>W3.CSS Template</title>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css" />
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway" />
+        <style dangerouslySetInnerHTML={{__html: "\nbody,h1,h5 {font-family: \"Raleway\", sans-serif}\nbody, html {height: 100%}\n.bgimg {\n  background-image: url('/w3images/onepage_restaurant.jpg');\n  min-height: 100%;\n  background-position: center;\n  background-size: cover;\n}\n" }} />
+        <div className="bgimg w3-display-container w3-text-white">
+          <div className="w3-display-middle w3-jumbo">
+            <p>logo</p>
+          </div>
+          <div className="w3-display-topleft w3-container w3-xlarge">
+            <p><button onclick="document.getElementById('menu').style.display='block'" className="w3-button w3-black">menu</button></p>
+            <p><button onclick="document.getElementById('contact').style.display='block'" className="w3-button w3-black">contact</button></p>
+          </div>
+          <div className="w3-display-bottomleft w3-container">
+            <p className="w3-xlarge">monday - friday 10-23 | saturday 14-02</p>
+            <p className="w3-large">42 village St, New York</p>
+            <p>powered by <a href="https://www.w3schools.com/w3css/default.asp" target="_blank">w3.css</a></p>
+          </div>
         </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a href="https://next.new" target="_blank" rel="noopener noreferrer">
-          Created with&nbsp;<b>next.new</b>&nbsp;⚡️
-        </a>
-      </footer>
-    </div>
-  );
-}
+        {/* Menu Modal */}
+        <div id="menu" className="w3-modal">
+          <div className="w3-modal-content w3-animate-zoom">
+            <div className="w3-container w3-black w3-display-container">
+              <span onclick="document.getElementById('menu').style.display='none'" className="w3-button w3-display-topright w3-large">x</span>
+              <h1>Starters</h1>
+            </div>
+            <div className="w3-container">
+              <h5>Tomato Soup <b>$2.50</b></h5>
+              <h5>Chicken Salad <b>$3.50</b></h5>
+              <h5>Bread and Butter <b>$1.00</b></h5>
+            </div>
+            <div className="w3-container w3-black">
+              <h1>Main Courses</h1>
+            </div>
+            <div className="w3-container">
+              <h5>Grilled Fish and Potatoes <b>$8.50</b></h5>
+              <h5>Italian Pizza <b>$5.50</b></h5>
+              <h5>Veggie Pasta <b>$4.00</b></h5>
+              <h5>Chicken and Potatoes <b>$6.50</b></h5>
+              <h5>Deluxe Burger <b>$5.00</b></h5>
+            </div>
+            <div className="w3-container w3-black">
+              <h1>Desserts</h1>
+            </div>
+            <div className="w3-container">
+              <h5>Fruit Salad <b>$2.50</b></h5>
+              <h5>Ice cream <b>$2.00</b></h5>
+              <h5>Chocolate Cake <b>$4.00</b></h5>
+              <h5>Cheese <b>$5.50</b></h5>
+            </div>
+          </div>
+        </div>
+        {/* Contact Modal */}
+        <div id="contact" className="w3-modal">
+          <div className="w3-modal-content w3-animate-zoom">
+            <div className="w3-container w3-black">
+              <span onclick="document.getElementById('contact').style.display='none'" className="w3-button w3-display-topright w3-large">x</span>
+              <h1>Contact</h1>
+            </div>
+            <div className="w3-container">
+              <p>Reserve a table, ask for today's special or just send us a message:</p>
+              <form action="/action_page.php" target="_blank">
+                <p><input className="w3-input w3-padding-16 w3-border" type="text" placeholder="Name" required name="Name" /></p>
+                <p><input className="w3-input w3-padding-16 w3-border" type="number" placeholder="How many people" required name="People" /></p>
+                <p><input className="w3-input w3-padding-16 w3-border" type="datetime-local" placeholder="Date and time" required name="date" defaultValue="2020-11-16T20:00" /></p>
+                <p><input className="w3-input w3-padding-16 w3-border" type="text" placeholder="Message \ Special requirements" required name="Message" /></p>
+                <p><button className="w3-button" type="submit">SEND MESSAGE</button></p>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+});
